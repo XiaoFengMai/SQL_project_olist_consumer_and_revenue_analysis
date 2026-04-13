4b69cb9c671b9d210746debb5087c9de88ea66ac
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-04-13 10:52:25 -0400

    Update 03_create_clean_view.sql

--- 03_create_clean_view.sql
+++ 03_create_clean_view.sql
@@ -4,3 +4,7 @@ SELECT * 			-- selects all columns from the orders table
 FROM orders			-- pulls from your raw orders table
 WHERE order_status = 'delivered'		--only keeps orders that were actually delivered
   AND order_purchase_timestamp IS NOT NULL;		-- removes any rows with missing date, only keeps rows with timestamps, needed for time-based analysis
+
+
+-- Verify
+SELECT COUNT(*) FROM clean_orders; -- Should be ~96k rows
